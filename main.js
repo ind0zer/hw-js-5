bcc216d59c7584a4b89aa0da7a3e07a0e27689f5
ind0zer <165696434+ind0zer@users.noreply.github.com>
2024-06-10 04:34:09 +0300

    switch

--- main.js
+++ main.js
@@ -1,90 +1,137 @@
-const select = prompt("Кава, Чай чи Сік?")
+const select = prompt("Кава, Чай чи Сік?");
 
-if (select === "Чай") {
+switch (select) {
+  case "Чай":
     alert(select);
-} else if (select === "Кава") {
+    break;
+
+  case "Кава":
     alert(select);
-} else if (select === "Сік") {
+    break;
+
+  case "Сік":
     alert(select);
-} else {
+    break;
+
+  default:
     alert("Введено невірний напій");
 }
 
+if (select === "Чай") {
+  alert(select);
+} else if (select === "Кава") {
+  alert(select);
+} else if (select === "Сік") {
+  alert(select);
+} else {
+  alert("Введено невірний напій");
+}
 
 const day = prompt("Введіть день тижня: ");
 
-if (day == "Понеділок") {
-    alert("Робочий день"); 
-} else if (day === "Понеділок") {
+switch (day) {
+  case "Понеділок":
     alert("Робочий день");
-} else if (day === "Вівторок") {
+    break;
+
+  case "Вівторок":
     alert("Робочий день");
-} else if (day === "Середа") {
+    break;
+
+  case "Середа":
     alert("Робочий день");
-} else if (day === "Четвер") {
+    break;
+
+  case "Четвер":
     alert("Робочий день");
-} else if (day === "П`ятниця") {
+    break;
+
+  case "П`ятниця":
     alert("Робочий день");
-} else if (day === "Суббота") {
+    break;
+
+  case "Суббота":
     alert("Вихідний");
-} else if (day === "Неділя") {
+    break;
+
+  case "Неділя":
     alert("Вихідний");
-} else {
-    alert("В неділі тільки 7 днів!")
+    break;
+
+  default:
+    alert("В неділі тільки 7 днів!");
 }
 
 const month = Number(prompt("Номер місяця: "));
 
-if (month === 1 || month === 2 || month === 12) {
+switch (month) {
+  case 1:
+  case 2:
+  case 12:
     alert("Зима");
-} else if (month === 3 || month === 4 || month === 5) {
+    break;
+
+  case 3:
+  case 4:
+  case 5:
     alert("Весна");
-} else if (month === 6 || month === 7 || month === 8) {
+    break;
+
+  case 6:
+  case 7:
+  case 8:
     alert("Літо");
-} else if (month === 9 || month === 10 || month === 11) {
+    break;
+
+  case 9:
+  case 10:
+  case 11:
     alert("Осінь");
-} else {
+    break;
+
+  default:
     alert("В році всього 12 місяців!");
 }
 
 const monthes = Number(prompt("Номер місяця: "));
 
-if (monthes === 1) {
+switch (monthes) {
+  case 1:
+  case 3:
+  case 5:
+  case 7:
+  case 8:
+  case 10:
+  case 12:
     alert("В цьому місяці 31 день");
-} else if (monthes === 2) {
-    alert("В цьому місяці 29 день");
-} else if (monthes === 3) {
-    alert("В цьому місяці 31 день");
-} else if (monthes === 4) {
+    break;
+  case 4:
+  case 6:
+  case 9:
+  case 11:
     alert("В цьому місяці 30 день");
-} else if (monthes === 5) {
-    alert("В цьому місяці 31 день");
-} else if (monthes === 6) {
-    alert("В цьому місяці 30 день");
-} else if (monthes === 7) {
-    alert("В цьому місяці 31 день");
-} else if (monthes === 8) {
-    alert("В цьому місяці 31 день");
-} else if (monthes === 9) {
-    alert("В цьому місяці 30 день");
-} else if (monthes === 10) {
-    alert("В цьому місяці 31 день");
-} else if (monthes === 11) {
-    alert("В цьому місяці 30 день");
-} else if (monthes === 12) {
-    alert("В цьому місяці 31 день");
-} else {
-    alert("В році всього 12 місяців!");
+    break;
+  case 2:
+    alert("В цьому місяці 28 день");
+    break;
+  default:
+    alert("Цього місяця не існує");
 }
 
 const color = prompt("Введіть колір світлофору: ");
 
-if (color === "Червоний") {
+switch (color) {
+  case "Червоний":
     alert("Червоний — стоп");
-} else if (color === "Зелений") {
+    break;
+
+  case "Зелений":
     alert("Зелений — йти");
-} else if (color === "Жовтий") {
+    break;
+
+  case "Жовтий":
     alert("Жовтий — чекати");
+    break;
 }
 
 const firstNumber = Number(prompt("Введіть перше число: "));
@@ -93,16 +140,25 @@ const secondNumber = Number(prompt("Введіть друге число: "));
 
 let result;
 
-if (calc === "+") {
+switch (calc) {
+  case "+":
     result = firstNumber + secondNumber;
-} else if (calc === "-") {
+    break;
+
+  case "-":
     result = firstNumber - secondNumber;
-} else if (calc === "*") {
+    break;
+
+  case "*":
     result = firstNumber * secondNumber;
-} else if (calc === "/") {
+    break;
+
+  case "/":
     result = firstNumber / secondNumber;
-} else {
-    result = "Невірна дія";
+    break;
+
+  default:
+    "Цю дію не можна використати";
 }
 
 alert(`Результат: ${result}`);
